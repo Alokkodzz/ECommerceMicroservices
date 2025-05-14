a845134df2c9ae4410f2c43433019fc7df29bc8d
Alokkodzz <alok635790@gmail.com>
2025-05-14 09:42:14 +0000

    product is working

--- Services/OrderService/Program.cs
+++ Services/OrderService/Program.cs
@@ -1,12 +1,45 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
+// Use CORS policy
+app.UseCors("AllowAll");
+
+// Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// Global OPTIONS handler for CORS preflight
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Options)
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.CompleteAsync();
+    }
+    else
+    {
+        await next();
+    }
+});
+
+// Map controllers
 app.MapControllers();
+
+// Run the application
 app.Run();
